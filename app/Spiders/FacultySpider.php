4745e2eb2a7c5c4488be668c5370a550cc28b102
MichalMyskow <michal.myskow@blumilk.pl>
2022-10-17 23:13:38 +0200

    #4 - fix phpstan

--- app/Spiders/FacultySpider.php
+++ app/Spiders/FacultySpider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Spiders;
 
 use Generator;
-use JetBrains\PhpStorm\NoReturn;
 use RoachPHP\Http\Response;
 use RoachPHP\Spider\BasicSpider;
 use Symfony\Component\DomCrawler\Crawler;
@@ -17,7 +16,6 @@ class FacultySpider extends BasicSpider
         "http://www.plan.collegiumwitelona.pl/",
     ];
 
-    #[NoReturn]
     public function parse(Response $response): Generator
     {
         $faculties = $response->filter("li")->slice(0, 3)->each(function (Crawler $crawler): array {
