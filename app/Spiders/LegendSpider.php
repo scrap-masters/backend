55045a6cd09017cb091f101c712184f99d1f9eff
Aleksander Kowalski <mrhdolek14@gmail.com>
2022-11-01 21:41:04 +0100

    feat: add scraping legend

--- app/Spiders/LegendSpider.php
+++ app/Spiders/LegendSpider.php
@@ -9,11 +9,19 @@ use RoachPHP\Http\Response;
 
 class LegendSpider extends Spider
 {
+    private const SELECTOR_TO_PLAN_LEGEND = "div#prtleg td";
+
     public function parse(Response $response): Generator
     {
-        $title = $response->filter("h1")->text();
-
-        dump($title);
-        yield $this->item([$title]);
+        $legends = $response->filter(self::SELECTOR_TO_PLAN_LEGEND);
+        $count = $legends->count();
+        for ($i = 0; $i < $count; $i++) {
+            // Todo: Add maping data for Dto and build collections items for this
+            // Todo: Add procesor to process data from dto to model and save
+            yield $this->item([
+                "slug" => $legends->getNode($i)->textContent,
+                "full_name" => $legends->getNode($i)->textContent,
+            ]);
+        }
     }
 }
