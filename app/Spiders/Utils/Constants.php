3bb63c7b554a498d8b91aa5ae29d0cb46019bd94
MichalMyskow <michal.myskow@blumilk.pl>
2022-11-16 22:11:00 +0100

    - fix specialization scraping

--- app/Spiders/Utils/Constants.php
+++ app/Spiders/Utils/Constants.php
@@ -16,7 +16,7 @@ class Constants
     public const SELECTOR_TO_HOURS = "td.godzina";
     public const SELECTOR_TO_FACULTY_LIST = "//div[@class='page-sidebar']//li/a[contains(text(), 'Wydzia')]";
     public const SELECTOR_TO_FIELD_LIST = "//ul[@class='accordion dark']//li/a";
-    public const SELECTOR_TO_SPECIALIZATION_LIST = "//ul[@class='accordion dark']//li/div/a[contains(text(), '(')]";
+    public const SELECTOR_TO_SPECIALIZATION_LIST = "//ul[@class='accordion dark']//li/div/a[contains(text(),";
     public const SPECIALIZATIONS_SLUG = "specializationSlug";
     public const FORMAT_DATE = "Y-m-d";
 }
