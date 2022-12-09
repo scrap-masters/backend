87457cd6d88ee27ef6310ede8e1f29b12b4113af
Aleksander Kowalski <mrhdolek14@gmail.com>
2022-12-09 22:00:01 +0100

    feat: add cache trait

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Models\Traits\CamelCaseAttributes;
+use App\Models\Utils\Constants;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 abstract class Model extends EloquentModel
 {
     use CamelCaseAttributes;
+    use QueryCacheable;
+
+    protected int $cacheFor = Constants::HALF_AN_HOUR;
+    protected string $cacheDriver = "redis";
+    protected static bool $flushCacheOnUpdate = true;
 
     public function asJson($value): bool|string
     {
